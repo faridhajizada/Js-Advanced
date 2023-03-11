69eb20a8b3b667469296f43b9842a8292f765085
Farid0558475484 <faridmh@code.edu.az>
2023-03-11 14:58:22 +0400

    d

---
+++ function.js
@@ -0,0 +1,51 @@
+//  ----------------- 1 Function Declaration -----------------
+function greet1(name) {
+  console.log(`Hello ${name}`);
+}
+
+// ----------------- 2 Function Expression -----------------
+
+const greet2 = function (name) {
+  console.log(`Hello ${name}`);
+};
+
+// ----------------- 3 Anonymous function -----------------
+
+// let counter = 0;
+// const interval = setInterval(function() {
+//     if(counter === 5) {
+//         clearInterval(interval)
+//     }else{
+//         console.log(++counter);
+//     }
+
+// }, 100);
+
+// greet1("Farid1");
+// greet2("Farid2");
+
+// console.dir(greet1);
+// console.dir(greet2);
+
+// ----------------- 4 Arrow function ------------------
+
+const pow2 = (num) => num ** 2;
+
+// console.log(pow2(5));
+
+// ----------------- 5 function Parametr ------------------
+
+const sum = (a = 1, b = 2) => a + b;
+
+// console.log(sum());
+
+
+const sumAll = (...all) => {
+  let result = 0;
+  for (let num of all) {
+    result += num;
+  }
+  console.log(result);
+};
+
+sumAll(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
