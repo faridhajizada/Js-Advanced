4002bbd6bd5dec6f9603b0502d60ba745f075240
Farid0558475484 <faridmh@code.edu.az>
2023-03-14 16:36:39 +0400

    feat:for while do while

--- for.js
+++ for.js
@@ -32,3 +32,88 @@
 //   console.log(i);
 //   i++;
 // }
+
+
+// var cats = ['Билл', 'Макс', 'Пикси', 'Алиса', 'Жасмин'];
+// var info = 'Моих кошек зовут ';
+// var para = document.querySelector('p');
+
+
+// for(let i=0; i <cats.length;i++){
+//     // info += cats[i] + ', ';
+
+//     if(i===cats.length-1){
+//         info= info + 'и ' + cats[i] + '.';
+
+//     }else{
+//         info= info + cats[i] + ', ';
+//     }
+// }
+// para.textContent = info;
+
+
+
+
+
+// var contacts = ['Григорий:2232322', 'Марина:3453456', 'Василий:7654322', 'Наталья:9998769', 'Диана:9384975'];
+// var para = document.querySelector('p');
+// var input = document.querySelector('input');
+// var btn = document.querySelector('button');
+
+// btn.addEventListener('click', function() {
+//   var searchName = input.value;
+//   input.value = '';
+//   input.focus();
+//   for (var i = 0; i < contacts.length; i++) {
+//     var splitContact = contacts[i].split(':');
+//     if (splitContact[0] === searchName) {
+//       para.textContent = splitContact[0] + ', тел.: ' + splitContact[1] + '.';
+//       break;
+//     } else {
+//       para.textContent = 'Контакт не найден.';
+//     }
+//   }
+// });
+
+
+
+
+// var n = 0;
+// var x = 0;
+
+// while (n < 3) {
+//   n++;
+//   x += n;
+// }
+
+// var a =1;
+// var a = 2;
+// console.log(a);
+
+// let z =1;
+// // let z = 2;
+// console.log(z);
+
+// var i = 0;
+
+// while (i < cats.length) {
+//   if (i === cats.length - 1) {
+//     info += 'and ' + cats[i] + '.';
+//   } else {
+//     info += cats[i] + ', ';
+//   }
+
+//   i++;
+// }
+
+// for (let i = 0; i < 5; i++) {
+//     console.log(i);
+//   }
+
+
+//   let i = 0;
+// while (i < 5) {
+//   console.log(i);
+//   i++;
+// }
+// // console.log(i); // 5
