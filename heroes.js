0bfe4255b23313cc2ffbda9d5cfb460d8e2850c1
Farid0558475484 <faridmh@code.edu.az>
2023-03-16 13:27:05 +0400

    start ts

---
+++ heroes.js
@@ -0,0 +1,8 @@
+// var header = document.querySelector('header');
+// var section = document.querySelector('section');
+
+
+let num = 3;
+num = 4;
+
+console.log(num);
---
+++ typescript.ts
@@ -0,0 +1,18 @@
+let isBoolean: boolean = true;
+// isBoolean = 1;
+// isBoolean = "true";
+isBoolean = true;
+
+let num1: number = 1;
+// num = true;
+// num = "1";
+num1 = 2;
+
+let str1: string = "hello";
+// str1 = 1;
+// str1 = true;
+str1 = "world";
+
+const greetUser = (): void => {
+  alert("Hello World");
+};
