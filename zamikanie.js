6df4d5a72a0b4f98552ed9fc23c39b948ca50dbe
Farid0558475484 <faridmh@code.edu.az>
2023-04-03 15:45:38 +0400

    feat:apply bind zamikanie

--- zamikanie.js
+++ zamikanie.js
@@ -7,5 +7,21 @@ function urlGenerator(domain) {
 const comUrl = urlGenerator("com");
 const ruUrl = urlGenerator("ru");
 
-console.log(comUrl("google"));
-console.log(comUrl("netflix"));
+// console.log(comUrl("google"));
+// console.log(comUrl("netflix"));
+
+function bind(context, fn) {
+  return function (...args) {
+    fn.apply(context, args);
+  };
+}
+
+function logPerson() {
+  console.log(`Person: ${this.name}, ${this.age}, ${this.job}`);
+}
+
+const person1 = { name: "Mikhail", age: 22, job: "Frontend" };
+const person2 = { name: "Elena", age: 19, job: "SMM" };
+
+bind(person1, logPerson)();
+bind(person2, logPerson)();
