07f848828f0a54d9e3bcec961212135e669587f1
Farid0558475484 <faridmh@code.edu.az>
2023-04-04 00:52:45 +0400

    feat:async await

--- async.js
+++ async.js
@@ -58,15 +58,52 @@
 // hello().then((value) => console.log(value))
 // hello().then(console.log)
 //
-var myGeo = navigator.geolocation;
-myGeo.getCurrentPosition(function(position) {
-    var latlng = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-    var myOptions = {
-      zoom: 8,
-      center: latlng,
-      mapTypeId: google.maps.MapTypeId.TERRAIN,
-      disableDefaultUI: true
-    }
-    var map = new google.maps.Map(document.querySelector("#map_canvas"), myOptions);
-  });
-  
+// var myGeo = navigator.geolocation;
+// myGeo.getCurrentPosition(function(position) {
+//     var latlng = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+//     var myOptions = {
+//       zoom: 8,
+//       center: latlng,
+//       mapTypeId: google.maps.MapTypeId.TERRAIN,
+//       disableDefaultUI: true
+//     }
+//     var map = new google.maps.Map(document.querySelector("#map_canvas"), myOptions);
+//   });
+
+const delay = (ms) => {
+  return new Promise((r) => setTimeout(() => r(), ms));
+};
+
+delay(2000).then(() => console.log("2 second"));
+
+const url = "https://jsonplaceholder.typicode.com/todos/1";
+
+// function fetchTodos() {
+//   console.log("Fetch todo started...");
+//   return delay(2000)
+//     .then(() => fetch(url))
+//     .then((response) => response.json());
+// }
+
+// fetchTodos()
+//   .then((data) => {
+//     console.log("Data: ", data);
+//   }
+//   )
+//   .catch((e) => console.error(e));
+
+async function fetchAsyncTodos() {
+  console.log("--------------------------------------------...");
+  console.log("Fetch todo started...");
+  try {
+    await delay(2000);
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log("Data: ", data);
+  } catch (e) {
+    console.error(e);
+  } finally {
+  }
+}
+
+fetchAsyncTodos();
