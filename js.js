828271e96ada8c7e09e492c14eed50e78541692d
Farid0558475484 <faridmh@code.edu.az>
2023-04-04 01:53:55 +0400

    feat:type js

---
+++ js.js
@@ -0,0 +1,75 @@
+// console.log('3' * '8' +4 +"salam");
+
+// console.log(null+2);
+// console.log(undefined+2);
+
+// console.log(false == 0);
+// console.log(false == '');
+// console.log(''== 0);
+// console.log(''== 0);
+// console.log(''== false);
+
+// console.log(''== null);
+// console.log(''=== null);
+// console.log(''== undefined);
+// console.log({}== null);
+// console.log({}== undefined);    
+// console.log({}== false);
+// console.log({}== 0);
+// console.log({}== '');
+// console.log({}== []);
+// console.log({}== {});
+// console.log({}== NaN);
+
+// console.log(0== null);
+// console.log(0== undefined);
+// console.log(0== false);
+// console.log(0== '');
+// console.log(0== []);
+// console.log(0== {});
+// console.log(0== NaN);
+
+// console.log(NaN== null);
+// console.log(NaN== undefined);
+// console.log(NaN== false);
+// console.log(NaN== '');
+// console.log(NaN== []);
+// console.log(NaN== {});
+// console.log(NaN=== NaN);
+
+// console.log([]== null);
+// console.log([]== undefined);
+// console.log([]== false);
+// console.log([]== '');
+// console.log([]== []);
+// console.log([]== {});
+// console.log([]== NaN);
+
+// console.log(null== undefined);
+// console.log(null== false);
+// console.log(null== '');
+// console.log(null== []);
+// console.log(null== {});
+// console.log(null== NaN);
+
+// console.log(undefined== false);
+// console.log(undefined== '');
+// console.log(undefined== []);
+// console.log(undefined== {});
+// console.log(undefined== NaN);
+
+// console.log(false== '');
+// console.log(false== []);
+// console.log(false== {});
+// console.log(false== NaN);
+
+// console.log(''== []);
+// console.log(''== {});
+// console.log(''== NaN);
+
+// console.log([]== {});
+// console.log([]== NaN);
+
+// console.log({}== NaN);
+
+
