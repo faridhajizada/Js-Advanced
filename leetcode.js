af09cf7c66aa119b89a0222f7bf663f99fb50755
Farid0558475484 <faridmh@code.edu.az>
2023-05-08 23:30:44 +0400

    feat:leetcode sleep easy

--- leetcode.js
+++ leetcode.js
@@ -7,11 +7,13 @@ var findDifference = function (nums1, nums2) {
 };
 
 console.log(findDifference([1, 2, 3, 4, 5], [1, 2, 3, 4]));var findDifference = function (nums1, nums2) {
-    const set1 = new Set(nums1);
-    const set2 = new Set(nums2);
-    const notInNums2 = Array.from(set1).filter((num) => !set2.has(num));
-    const notInNums1 = Array.from(set2).filter((num) => !set1.has(num));
-    return [notInNums2, notInNums1];
-  };
-  
-  console.log(findDifference([1, 2, 3, 4, 5], [1, 2, 3, 4]));
+
+
+
+
+  async function sleep(millis) {
+    await new Promise(res=>setTimeout(res,millis))
+ }
+
+ 
+ //sleep leetcode
