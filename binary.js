91c50bd108d831fc8eee73e222001bb13b6e32d9
Farid0558475484 <faridmh@code.edu.az>
2023-08-07 00:14:16 +0400

    feat: update binary

--- binary.js
+++ binary.js
@@ -2,20 +2,20 @@ console.log("Hello World");
 
 const arr = [1, 4, 7, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45];
 
-let left = 0;
-let right = arr.length - 1;
-let mid = Math.round((left + right) / 2);
-
 const BinarySearch = (arr, value) => {
-  while (arr[mid] !== value && left <= right) {
-    if (value > arr[mid]) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left <= right) {
+    let mid = Math.round((left + right) / 2);
+    if (arr[mid] === value) {
+      return mid;
+    } else if (value > arr[mid]) {
       left = mid + 1;
     } else {
       right = mid - 1;
     }
-    mid = Math.round((left + right) / 2);
   }
-  return arr[mid] === value ? mid : -1;
 };
 
-console.log(BinarySearch(arr, 1));
+console.log(BinarySearch(arr, 12));
