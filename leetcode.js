47b4f5e3d03769d3867ecf98142d9bccf20670fa
Farid0558475484 <faridmh@code.edu.az>
2023-08-20 16:51:04 +0400

    Leetcode : subtractProductAndSum and others

--- leetcode.js
+++ leetcode.js
@@ -264,20 +264,49 @@
 //   }
 // };
 
-const nums = [1, 2, 3, 1, 1, 3];
+// const nums = [1, 2, 3, 1, 1, 3];
 
-const numIdenticalPairs = function (nums) {
-  let count = 0;
+// const numIdenticalPairs = function (nums) {
+//   let count = 0;
 
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] === nums[j]) {
-        console.log(nums[i], nums[j]);
-        count++;
-      }
-    }
-  }
-  return count;
-};
+//   for (let i = 0; i < nums.length; i++) {
+//     for (let j = i + 1; j < nums.length; j++) {
+//       if (nums[i] === nums[j]) {
+//         console.log(nums[i], nums[j]);
+//         count++;
+//       }
+//     }
+//   }
+//   return count;
+// };
 
-console.log(numIdenticalPairs(nums));
+// console.log(numIdenticalPairs(nums));
+
+// const nums = 234756;
+
+// const subtractProductAndSum = function (nums) {
+//   let digit = 1;
+//   let sum = 0;
+//   let result = 0;
+//   const numStr = nums.toString();
+
+//   for (let i = 0; i < numStr.length; i++) {
+//     digit *= Number(numStr[i]);
+//     sum += Number(numStr[i]);
+//   }
+//   result = digit - sum;
+
+//   return result;
+// };
+
+// console.log(subtractProductAndSum(nums));
+
+// function fac(n) {
+//   if (n === 0 || n === 1) {
+//     return 1;
+//   }
+
+//   return n * fac(n - 1);
+// }
+
+// console.log(fac(5));
